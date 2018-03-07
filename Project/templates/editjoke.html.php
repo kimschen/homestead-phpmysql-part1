064c695518c03976168e862a5cc00a2983d59776
kimsphere <kimiley90@gmail.com>
2018-03-07 19:14:34 +0800

    Change name attribute to array

--- Project/templates/editjoke.html.php
+++ Project/templates/editjoke.html.php
@@ -1,11 +1,11 @@
 <form action="" method="post">
 
 	<input type="hidden"
-		   name="jokeid"
+		   name="joke[id]"
 		   value="<?=$joke['id'] ?? ''?>">
 
 	<label for="joketext">Type your joke here:</label>
-	<textarea id="joketext" name="joketext" rows="3" cols="40"><?=$joke['joketext'] ?? ''?></textarea>
+	<textarea id="joketext" name="joke[joketext]" rows="3" cols="40"><?=$joke['joketext'] ?? ''?></textarea>
 
 	<input type="submit" name="submit" value="Save">
 </form>
